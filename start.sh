bd060993f8597f59f478b3076908a0bbc2606e62
Leo BRUNET <leo.brunet@pradeo.com>
2025-01-22 11:39:14 +0100

    reset

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-./micromdm serve \
-  -server-url=https://stunning-space-tribble-7575v4rqq9j2xprj-8080.app.github.dev/ \
-  -api-key MySecureAPIKey12345 \
-  -filerepo . \
-  -tls=false \
-  -command-webhook-url=https://mdm-pradeo.onrender.com/webhook
